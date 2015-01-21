5881edf3a70370c67a4fa8c4d2b5de2f01a9fdbe
unknown <an.yablonski@gmail.com>
2015-01-21 16:13:03 +0200

    ReportsActivity and ComplaintsActivity splitted to one ReportsActivity with parameter

--- app/src/main/java/com/github/yablonski/majordom/MenuActivity.java
+++ app/src/main/java/com/github/yablonski/majordom/MenuActivity.java
@@ -14,53 +14,107 @@ public class MenuActivity extends ActionBarActivity {
     final String LOG_TAG = "myLogs";
 
     private ListView listView;
-    public String[] menu;
+    public static String[] menu;
+    public static String key;
+    private String title;
+
+    private enum Menu {
+        BOOKING(R.string.main_menu_booking),
+        MY_PROFILE(R.string.main_menu_profile),
+        MY_PACKAGES(R.string.main_menu_packages),
+        REPORTS(R.string.main_menu_reports),
+        COMPLAINTS(R.string.main_menu_complaints),
+        LOST_N_FOUND(R.string.main_menu_lostnfound),
+        NEWS(R.string.main_menu_news);
+
+        private int label;
+
+        Menu(int label) {
+            this.label = label;
+        }
+        public int getLabel() {
+            return label;
+        }
+    }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         listView = (ListView) findViewById(R.id.listView);
         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this, R.array.menu,
-                android.R.layout.simple_list_item_1);
-        listView.setAdapter(adapter);
         menu = getResources().getStringArray(R.array.menu);
+        MainMenuAdapter adapter = new MainMenuAdapter(this,menu);
+        listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            //MenuItem menuItem = MenuItem.valueOf;
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-                Log.d(LOG_TAG, "itemClick: position = " + position + ", id = "
-                        + id);
-                //TODO create enum's
-                switch (position) {
-                    case 0:
+
+                //String name = (String) parent.getItemAtPosition(position);
+                //Menu menuItem = Menu.valueOf(name);
+               // Log.d(LOG_TAG, "enum = " + name);
+                Menu menuItem = Menu.COMPLAINTS;
+                switch (menuItem) {
+                    case BOOKING:
                         Intent newActivity0 = new Intent(MenuActivity.this,BookActivity.class);
                         startActivity(newActivity0);
                         break;
-                    case 1:
+                    case MY_PACKAGES:
                         Intent intentPackages = new Intent(MenuActivity.this,PackagesActivity.class);
                         startActivity(intentPackages);
                         break;
-                    case 2:
+                    case REPORTS:
                         Intent intentReports = new Intent(MenuActivity.this,ReportsActivity.class);
+                        intentReports.putExtra(key,R.string.main_menu_reports);
                         startActivity(intentReports);
                         break;
-                    case 3:
-                        Intent intentComplaints = new Intent(MenuActivity.this,ComplaintsActivity.class);
+                    case COMPLAINTS:
+                        Intent intentComplaints = new Intent(MenuActivity.this,ReportsActivity.class);
+                        intentComplaints.putExtra(key,R.string.main_menu_complaints);
                         startActivity(intentComplaints);
                         break;
-                    case 4:
+                    case LOST_N_FOUND:
                         Intent newActivity4 = new Intent(MenuActivity.this,NewsActivity.class);
                         startActivity(newActivity4);
                         break;
-                    case 5:
+                    case NEWS:
                         Intent intent = new Intent(MenuActivity.this, NewsActivity.class);
                         startActivity(intent);
                         break;
-                    default:
-                        // Nothing do!
+                    case MY_PROFILE:
+                        Intent intentProfile = new Intent(MenuActivity.this, NewsActivity.class);
+                        startActivity(intentProfile);
+                        break;
                 }
+                //TODO create enum's
+
+                /*switch (name) {
+                    case BOOKING:
+                        Intent newActivity0 = new Intent(MenuActivity.this,BookActivity.class);
+                        startActivity(newActivity0);
+                        break;
+                    case MY_PACKAGES:
+                        Intent intentPackages = new Intent(MenuActivity.this,PackagesActivity.class);
+                        startActivity(intentPackages);
+                        break;
+                    case REPORTS:
+                        Intent intentReports = new Intent(MenuActivity.this,ReportsActivity.class);
+                        startActivity(intentReports);
+                        break;
+                    case COMPLAINTS:
+                        Intent intentComplaints = new Intent(MenuActivity.this,ComplaintsActivity.class);
+                        startActivity(intentComplaints);
+                        break;
+                    case LOST_N_FOUND:
+                        Intent newActivity4 = new Intent(MenuActivity.this,NewsActivity.class);
+                        startActivity(newActivity4);
+                        break;
+                    case NEWS:
+                        Intent intent = new Intent(MenuActivity.this, NewsActivity.class);
+                        startActivity(intent);
+                        break;
+                }*/
             }
         });
     }
--- app/src/main/java/com/github/yablonski/majordom/ReportsActivity.java
+++ app/src/main/java/com/github/yablonski/majordom/ReportsActivity.java
@@ -1,17 +1,32 @@
 package com.github.yablonski.majordom;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class ReportsActivity extends ActionBarActivity {
 
+    private String TAG = "my_TAG";
+    private int id, titleId;
+    private String title, message = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        titleId = intent.getIntExtra(MenuActivity.key,id);
+        title = getString(titleId);
+        setTitle(title);
         setContentView(R.layout.activity_reports);
+        if (id == R.string.main_menu_reports) {
+            message = "Please write your report here.\nWe will solve it as soon as possible.";
+        } else {
+            message = "Please write your complaint here.\nWe will solve it as soon as possible.";
+        }
+        TextView titleReportTextView = (TextView) findViewById(R.id.titleReportTextView);
+        titleReportTextView.setText(message);
     }
 
 }
